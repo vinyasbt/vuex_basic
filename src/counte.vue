4dbeae8fc032e51d9527e070e7e79df812b3086b
vinyasbt <vinyas.bt27@gmail.com>
2020-10-06 16:23:57 +0530

    basic vuex

---
+++ src/counte.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        {{count}}
+        <button @click="increment()">+</button>
+        <button @click="decrement()">-</button>
+    </div>
+</template>
+<script>
+export default {
+    name:"counte",
+   
+    methods:{
+        increment(){
+            this.$store.commit("increment")
+        },
+         decrement(){
+            this.$store.commit("decrement")
+        }
+    },
+    computed:{
+        count(){
+            return this.$store.state.count
+        }
+    }
+}
+</script>
